d31d941ec09ff0f5e378082df838a30fe79d1474
jvctrsz <joaosteam26@gmail.com>
2026-06-13 15:00:41 -0400

    remove arquivos default

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { SalariesModule } from './salaries/salaries.module';
 
 @Module({
   imports: [SalariesModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
